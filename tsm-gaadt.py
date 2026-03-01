e8ad35ba19552fc00b7d9cd4de7818f3d3808ddb
José Leandro <jleandro.silva@edge.ufal.br>
2026-03-01 20:40:47 -0300

    Fix best fitness attribute and improve genetic operators

--- tsm-gaadt.py
+++ tsm-gaadt.py
@@ -4,40 +4,24 @@ import random
 from sys import maxsize as MAX_INT
 from collections import deque
 
-DEFAULT_GENE_LENGTH = 10
-DEFAULT_POPULATION_SIZE = 10
-DEFAULT_GENERATION_COUNT = 20
+DEFAULT_GENE_LENGTH = 5
+DEFAULT_POPULATION_SIZE = 5
+DEFAULT_GENERATION_COUNT = 200
 
 
 MUTATION_RATE = 0.1
 
-CITIES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# CITIES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+CITIES = [1, 2, 3, 4, 5]
 DISTANCE_MATRIX = {
-    (1, 2): 10, (1, 3): 15, (1, 4): 20, (1, 5): 25, (1, 6): 30, (1, 7): 35, (1, 8): 40, (1, 9): 45, (1, 10): 50,
-    (2, 3): 35, (2, 4): 25, (2, 5): 30, (2, 6): 20, (2, 7): 15, (2, 8): 40, (2, 9): 45, (2, 10): 50, (2, 1): 100,
-    (3, 4): 30, (3, 5): 20, (3, 6): 25, (3, 7): 35, (3, 8): 40, (3, 9): 45, (3, 10): 50, (3, 1): 100, (3, 2): 95,
-    (4, 5): 15, (4, 6): 30, (4, 7): 25, (4, 8): 35, (4, 9): 40, (4, 10): 45, (4, 1): 100, (4, 2): 95, (4, 3): 90,
-    (5, 6): 20, (5, 7): 30, (5, 8): 25, (5, 9): 35, (5, 10): 40, (5, 1): 100, (5, 2): 95, (5, 3): 90, (5, 4): 85,
-    (6, 7): 15, (6, 8): 20, (6, 9): 30, (6, 10): 35, (6, 1): 100, (6, 2): 95, (6, 3): 90, (6, 4): 85, (6, 5): 80,
-    (7, 8): 10, (7, 9): 25, (7, 10): 30, (7, 1): 100, (7, 2): 95, (7, 3): 90, (7, 4): 85, (7, 5): 80, (7, 6): 75,
-    (8, 9): 15, (8, 10): 20, (8, 7): 100,
-    (9, 10): 10, (9, 1): 55, (9, 2): 50, (9, 3): 45, (9, 4): 40, (9, 5): 35, (9, 6): 30, (9, 7): 25, (9, 8): 20,
-    (10, 1): 55, (10, 2): 50, (10, 3): 45, (10, 4): 40, (10, 5): 35, (10, 6): 30, (10, 7): 25, (10, 8): 20, (10, 9): 15
+    (1, 2): 10, (1, 3): 15, (1, 4): 20, (1, 5): 1,
+    (2, 3): 35, (2, 4): 25, (2, 5): 30, (2, 1): 1,
+    (3, 4): 30, (3, 5): MAX_INT, (3, 1): 25, (3, 2): 1,
+    (4, 5): 15, (4, 1): 30, (4, 2): 25, (4, 3): 1,
+    (5, 1): 20, (5, 2): 30, (5, 3): MAX_INT, (5, 4): 1,
 }
 
 children = []
-# DISTANCE_MATRIX = {
-#     (1, 2): 10, (1, 3): 15, (1, 4): 20, (1, 5): 25,
-#     (2, 3): 35, (2, 4): 25, (2, 5): 30,
-#     (3, 4): 30, (3, 5): 20,
-#     (4, 5): 15
-# }
-# DISTANCE_MATRIX = {
-#     (1, 2): 10, (1, 3): 15, (1, 4): 20, (1, 5): 25,
-#     (2, 1): 100, (2, 3): 35, (2, 4): 25, (2, 5): 30,
-#     (3, 4): 30, (3, 5): 20,
-#     (3, 4): 200, (4, 5): 15
-# }
 
 class Chromosome:
     def __init__(self, genes, fitness_function=sum):
@@ -51,10 +35,21 @@ class Chromosome:
     def calculate_fitness(self):
         return self.fitness_function(self.genes)
 
-    def mutate(self):
+    def mutate_old(self):
         index = random.randint(0, len(self.genes) - 1)
         city, alternate = random.sample(self.genes, 2)
         self.genes[index] = city if self.genes[index] != city else alternate
+        self.fitness = self.calculate_fitness()
+
+    def mutate(self):
+
+        for i in range(len(self.genes) - 1):
+            if random.random() < MUTATION_RATE:
+                j, alternate = random.sample(range(len(self.genes)), 2)
+                if(i == j):
+                    j = alternate
+                self.genes[i], self.genes[j] = self.genes[j], self.genes[i]
+
         self.fitness = self.calculate_fitness()
 
     def __str__(self):
@@ -69,9 +64,10 @@ class Chromosome:
 class Population:
     def __init__(self, size=DEFAULT_POPULATION_SIZE, gene_length=DEFAULT_GENE_LENGTH, fitness_function=sum):
         self.chromosomes = [Chromosome(random.sample(CITIES, gene_length), fitness_function) for _ in range(size)]
-        # self.chromosomes.append(Chromosome([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], fitness_function))
-        # self.chromosomes.append(Chromosome([10, 9, 8, 7, 6, 5, 4, 3, 2, 1], fitness_function))
-        self.best_chromosome = self.get_best_chromosome()
+
+    @property
+    def best_chromosome(self):
+        return self.get_best_chromosome()
 
     def evolve(self):
         self.chromosomes.sort(key=lambda x: x.fitness, reverse=True)
@@ -82,7 +78,6 @@ class Population:
         for _ in range(len(self.chromosomes)):
             parent1, parent2 = random.choices(parents, k=2)
             
-            # child1, child2 = self.crossover(parent1, parent2) 
             child1 = Chromosome(self.crossover_gaadt(parent1, parent2), tsm_fitness)
             child2 = Chromosome(self.crossover_gaadt(parent2, parent1), tsm_fitness)
 
@@ -106,26 +101,16 @@ class Population:
         # next_generation.append(Chromosome([1, 3, 4, 2, 10, 7, 8, 9, 5, 6], tsl_fitness))
         self.chromosomes.extend(next_generation)
         self.chromosomes = list(set(self.chromosomes)) # Remove duplicates
+
         # print([x.genes for x in self.chromosomes])
-        best = self.get_best_chromosome()
-        if self.best_chromosome is None or best.fitness < self.best_chromosome.fitness:
-            self.best_chromosome = best
+        # best = self.get_best_chromosome()
+        # if self.best_chromosome is None or best.fitness < self.best_chromosome.fitness:
+        #     self.best_chromosome = best
 
         # Population trimming based on average fitness
         # if(len(self.chromosomes) > DEFAULT_POPULATION_SIZE):
         #     average_fitness = sum([chromosome.fitness for chromosome in self.chromosomes]) / len(self.chromosomes)
         #     self.chromosomes = [chromosome for chromosome in self.chromosomes if chromosome.fitness <= average_fitness]
-
-
-    def crossover(self, parent1, parent2, crossover_point=None):
-        # print("p1", parent1.genes, "p2", parent2.genes)
-        if crossover_point is None:
-            crossover_point = random.randint(1, len(parent1.genes) - 1)
-
-        child1_genes = parent1.genes[:crossover_point] + parent2.genes[crossover_point:]
-        child2_genes = parent2.genes[:crossover_point] + parent1.genes[crossover_point:]
-
-        return Chromosome(child1_genes, parent1.fitness_function), Chromosome(child2_genes, parent2.fitness_function)
     
     def crossover_gaadt(self, parent1, parent2):
         genes_1 = parent1.genes
@@ -166,7 +151,7 @@ def tsm_fitness(genes):
     for i in range(len(genes) - 1):
         city1, city2 = genes[i], genes[i + 1]
         total_distance += DISTANCE_MATRIX.get((city1, city2), MAX_INT)
-    # total_distance += DISTANCE_MATRIX.get((genes[-1], genes[0]), MAX_INT)  # Return to starting city
+    total_distance += DISTANCE_MATRIX.get((genes[-1], genes[0]), MAX_INT)
     return total_distance
     
 if(__name__ == "__main__"):
--- tsm-holland.py
+++ tsm-holland.py
@@ -4,24 +4,10 @@ import random
 from sys import maxsize as MAX_INT
 
 DEFAULT_GENE_LENGTH = 5
-DEFAULT_POPULATION_SIZE = 25
-DEFAULT_GENERATION_COUNT = 1000
+DEFAULT_POPULATION_SIZE = 5
+DEFAULT_GENERATION_COUNT = 200
 
 MUTATION_RATE = 0.1
-
-# CITIES = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-# DISTANCE_MATRIX = {
-#     (1, 2): 10, (1, 3): 15, (1, 4): 20, (1, 5): 25, (1, 6): 30, (1, 7): 35, (1, 8): 40, (1, 9): 45, (1, 10): 50,
-#     (2, 3): 35, (2, 4): 25, (2, 5): 30, (2, 6): 20, (2, 7): 15, (2, 8): 40, (2, 9): 45, (2, 10): 50, (2, 1): 100,
-#     (3, 4): 30, (3, 5): 20, (3, 6): 25, (3, 7): 35, (3, 8): 40, (3, 9): 45, (3, 10): 50, (3, 1): 100, (3, 2): 95,
-#     (4, 5): 15, (4, 6): 30, (4, 7): 25, (4, 8): 35, (4, 9): 40, (4, 10): 45, (4, 1): 100, (4, 2): 95, (4, 3): 90,
-#     (5, 6): 20, (5, 7): 30, (5, 8): 25, (5, 9): 35, (5, 10): 40, (5, 1): 100, (5, 2): 95, (5, 3): 90, (5, 4): 85,
-#     (6, 7): 15, (6, 8): 20, (6, 9): 30, (6, 10): 35, (6, 1): 100, (6, 2): 95, (6, 3): 90, (6, 4): 85, (6, 5): 80,
-#     (7, 8): 10, (7, 9): 25, (7, 10): 30, (7, 1): 100, (7, 2): 95, (7, 3): 90, (7, 4): 85, (7, 5): 80, (7, 6): 75,
-#     (8, 9): 15, (8, 10): 20, (8, 7): 100, (8, 1): 100, (8, 2): 95, (8, 3): 90, (8, 4): 85, (8, 5): 80, (8, 6): 75,
-#     (9, 10): 10, (9, 1): 55, (9, 2): 50, (9, 3): 45, (9, 4): 40, (9, 5): 35, (9, 6): 30, (9, 7): 25, (9, 8): 20,
-#     (10, 1): 55, (10, 2): 50, (10, 3): 45, (10, 4): 40, (10, 5): 35, (10, 6): 30, (10, 7): 25, (10, 8): 20, (10, 9): 15
-# }
 
 CITIES = [1, 2, 3, 4, 5]
 DISTANCE_MATRIX = {
@@ -45,9 +31,15 @@ class Chromosome:
         return self.fitness_function(self.genes)
 
     def mutate(self):
-        index = random.randint(0, len(self.genes) - 1)
-        city, alternate = random.sample(self.genes, 2)
-        self.genes[index] = city if self.genes[index] != city else alternate
+
+        for i in range(len(self.genes) - 1):
+            if random.random() < MUTATION_RATE:
+                # j = random.randint(i + 1, len(self.genes) - 1)
+                j, alternate = random.sample(range(len(self.genes)), 2)
+                if(i == j):
+                    j = alternate
+                self.genes[i], self.genes[j] = self.genes[j], self.genes[i]
+
         self.fitness = self.calculate_fitness()
 
     def __str__(self):
@@ -61,10 +53,16 @@ class Chromosome:
     
 class Population:
     def __init__(self, size=DEFAULT_POPULATION_SIZE, gene_length=DEFAULT_GENE_LENGTH, fitness_function=sum, initial_city=None):
-        self.chromosomes = [self.generate_random_chromosome(gene_length, fitness_function, initial_city) for _ in range(size-2)]
-        self.chromosomes.append(Chromosome([5, 4, 1, 2, 3], fitness_function))
-        self.chromosomes.append(Chromosome([4, 5, 3, 2, 1], fitness_function))
-        self.best_chromosome = self.get_best_chromosome()
+        self.chromosomes = [self.generate_random_chromosome(gene_length, fitness_function, initial_city) for _ in range(size)]
+
+    @property
+    def best_chromosome(self):
+        return self.get_best_chromosome()
+
+    # @best_chromosome.setter
+    # def best_chromosome(self, value):
+    #     self._best_chromosome = value
+
 
     def generate_random_chromosome(self, gene_length, fitness_function, initial_city=None):
         if initial_city is not None:
@@ -76,15 +74,15 @@ class Population:
     def evolve(self):
         self.chromosomes.sort(key=lambda x: x.fitness, reverse=False)
         parents = self.chromosomes[:len(self.chromosomes)//2]  # Select the top half
-        print("Size of current population:", len(self.chromosomes))
-        print("Size of parents:", len(parents))
 
         next_generation = []
 
         for _ in range(len(self.chromosomes)):
             parent1, parent2 = random.choices(parents, k=2)
             
-            child1, child2 = self.crossover(parent1, parent2, 2) 
+            # child1, child2 = self.crossover(parent1, parent2, 2)
+            child1 = self.ordered_crossover(parent1, parent2) 
+            child2 = self.ordered_crossover(parent1, parent2) 
 
             if random.random() < MUTATION_RATE:
                 child1.mutate()
@@ -94,24 +92,15 @@ class Population:
 
             if self.are_genes_valid(child1.genes):
                 next_generation.append(child1)
-                # print("Valid child: ", child1)
 
             if self.are_genes_valid(child2.genes):
                 next_generation.append(child2)
-                # print("Valid child: ", child2)
-
-            # print(next_generation)
 
         self.chromosomes.extend(next_generation)
         self.chromosomes = list(set(self.chromosomes)) # Remove duplicates
-        # print([x.genes for x in self.chromosomes])
-        best = self.get_best_chromosome()
-        if self.best_chromosome is None or best.fitness > self.best_chromosome.fitness:
-            self.best_chromosome = best
 
 
     def crossover(self, parent1, parent2, crossover_point=None):
-        # print("p1", parent1.genes, "p2", parent2.genes)
         if crossover_point is None:
             crossover_point = random.randint(1, len(parent1.genes) - 1)
 
@@ -119,6 +108,22 @@ class Population:
         child2_genes = parent2.genes[:crossover_point] + parent1.genes[crossover_point:]
 
         return Chromosome(child1_genes, parent1.fitness_function), Chromosome(child2_genes, parent2.fitness_function)
+    
+    def ordered_crossover(self, parent1, parent2):
+        size = len(parent1.genes)
+        start, end = sorted(random.sample(range(size), 2))
+        child_genes = [None] * size
+
+        child_genes[start:end] = parent1.genes[start:end]
+
+        p2_index = 0
+        for i in range(len(child_genes)):
+            if child_genes[i] is None:
+                while parent2.genes[p2_index] in child_genes:
+                    p2_index += 1
+                child_genes[i] = parent2.genes[p2_index]
+
+        return Chromosome(child_genes, parent1.fitness_function)
     
     def are_genes_valid(self, genes):
         return sorted(genes) == sorted(CITIES[:len(genes)])
@@ -145,15 +150,11 @@ if(__name__ == "__main__"):
     population = Population(fitness_function=tsm_fitness, initial_city=5)
     initial = population
 
-    print("Initial Population:")
-    print(population)
-    print(f"Best Chromosome: {population.best_chromosome}")
-    print()
-
     initial_best = population.best_chromosome
+    print(initial_best)
     for generation in range(DEFAULT_GENERATION_COUNT):
         population.evolve()
-        print(f"Generation {generation + 1}: Best Chromosome: {population.best_chromosome}, Population Size: {len(population.chromosomes)}")
+        # print(f"Generation {generation + 1}: Best Chromosome: {population.best_chromosome}, Population Size: {len(population.chromosomes)}")
     
     print("initial population: \n", initial)
     print(f"\nInitial Best Chromosome: {initial_best}")
